d8176a1ff2de50cccf15b092cd18724f0162f328
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-07 14:43:34 +0200

    implement basic upload file configuration
    - required upload file will have custom configuration with testing requests to server
    - to avoid server load, will use this for now and complete live code on launching live version

--- src/features/AddAppointmentForm/AddAppointmentForm.tsx
+++ src/features/AddAppointmentForm/AddAppointmentForm.tsx
@@ -1,6 +1,15 @@
 import { FC } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Alert, Button, Col, Typography } from "antd";
+import {
+  Alert,
+  Button,
+  Col,
+  Typography,
+  Upload,
+  UploadProps,
+  message,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useTranslation } from "react-i18next";
@@ -223,11 +232,33 @@ const AddAppointmentForm: FC<AddAppointmentFormProps> = ({
     );
   };
 
+  const uploadProps: UploadProps = {
+    name: "file",
+    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+    maxCount: 1,
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
       style={{ direction: i18n.language == "ar" ? "rtl" : "ltr" }}
     >
+      <DualField />
+      <Upload {...uploadProps}>
+        <Button type="primary" icon={<UploadOutlined />}>
+          Upload Image
+        </Button>
+      </Upload>
+
       {renderItems(fieldsConfig)}
       <Button type="primary" htmlType="submit">
         submit
