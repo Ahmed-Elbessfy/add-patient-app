281656e11182ec18d1943a22a6a480f4b4f8d36b
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-07 15:24:59 +0200

    config empty fields & errors with checkbox same as switch
    - empty fields & clear errors on checkbox fields change same as switch field change
    same behavior as commit : cb349f229303109dbd7371a8dc54161c58958111 but for checkbox fields

--- src/features/CheckboxField/CheckboxField.tsx
+++ src/features/CheckboxField/CheckboxField.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 import { FieldCheckboxComponentProps } from "../AddAppointmentFields/AddAppointmentInputs.type";
 
 const CheckboxField: FC<FieldCheckboxComponentProps> = (props) => {
-  const { label, id, testId, onChange } = props;
+  const { label, id, testId, onChange, resetField } = props;
 
   const { t } = useTranslation("translation");
 
@@ -14,7 +14,18 @@ const CheckboxField: FC<FieldCheckboxComponentProps> = (props) => {
       data-testid={testId}
       id={id}
       defaultChecked={props.defaultChecked}
-      onChange={(e: CheckboxChangeEvent) => onChange(e.target.checked)}
+      onChange={(e: CheckboxChangeEvent) => {
+        onChange(e.target.checked);
+
+        // reset field accepts only one field
+        // reset method did not work properly
+        // keep error to false to remove errors in case of changing visibility
+        if (resetField && props.emptyFields) {
+          for (const field of props.emptyFields) {
+            resetField(field, { keepError: false });
+          }
+        }
+      }}
     >
       {label && t(label)}
     </Checkbox>
