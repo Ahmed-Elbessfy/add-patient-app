fa067348fb59b04259ad6ebf8e8859198c46c266
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-14 18:39:36 +0200

    style combine field

--- src/features/CombineField/CombineField.tsx
+++ src/features/CombineField/CombineField.tsx
@@ -7,15 +7,17 @@ const CombineField: FC<CombineFieldComponentProps> = (props) => {
   const { category, id, testId, fieldsConfig, label, renderFieldItems } = props;
   console.log(category, id, testId, fieldsConfig);
   return (
-    <>
+    <Col style={{ textAlign: "start" }} md={8}>
       <label>{label && t(label)}</label>
-      <Col>
-        <div style={{ display: "flex", width: "100%" }}>
-          <span style={{ flex: 1 }}>{renderFieldItems(fieldsConfig[0])}</span>
-          <span style={{ flex: 4 }}>{renderFieldItems(fieldsConfig[1])}</span>
-        </div>
-      </Col>
-    </>
+      <div style={{ display: "flex", width: "100%" }}>
+        <span style={{ flex: "20px", margin: " 0-8px" }}>
+          {renderFieldItems(fieldsConfig[0])}
+        </span>
+        <span style={{ flex: 5, margin: " 0-8px" }}>
+          {renderFieldItems(fieldsConfig[1])}
+        </span>
+      </div>
+    </Col>
   );
 };
 
