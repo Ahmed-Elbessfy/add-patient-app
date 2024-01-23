ffb5acebeebdf3456127a188d3d3c9e8dd83a910
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-23 13:11:37 +0200

    use InputNumber for numbers instead of Input
    - Bug: Number fields gives error of validating NaN if user
    entered value then deleted
    - Solution: use InputNumber instead of Input
    and register field value only if not null

--- src/features/NumberField/NumberField.tsx
+++ src/features/NumberField/NumberField.tsx
@@ -1,11 +1,10 @@
-import { ChangeEvent, FC } from "react";
-import { Input } from "antd";
+import { FC } from "react";
+import { InputNumber } from "antd";
 import { useTranslation } from "react-i18next";
 import { FieldNumberComponentProps } from "./NumberField.type";
 
 const NumberField: FC<FieldNumberComponentProps> = (props) => {
   const {
-    fieldType,
     label,
     placeholder,
     onChange,
@@ -24,8 +23,7 @@ const NumberField: FC<FieldNumberComponentProps> = (props) => {
     <>
       {label && <label>{t(label)}</label>}
 
-      <Input
-        type={fieldType}
+      <InputNumber
         placeholder={placeholder && t(placeholder)}
         status={status}
         disabled={isDisabled}
@@ -34,9 +32,12 @@ const NumberField: FC<FieldNumberComponentProps> = (props) => {
         addonBefore={prefix}
         min={min}
         max={max}
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          onChange(e.target.value)
-        }
+        style={{ width: "100%" }}
+        // null is required by InputNumber onChange method
+        onChange={(value: number | null) => {
+          // only submit if field is not empty - value not null
+          if (value) onChange(value);
+        }}
       />
     </>
   );
