aa669816108b955e7e511fd87499a9d407a3c53f
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-28 14:48:37 +0200

    add comments to Time picker Field type file

--- src/features/TimeField/TimeField.type.tsx
+++ src/features/TimeField/TimeField.type.tsx
@@ -14,16 +14,28 @@ export type FieldTimeT = Customize<
   }
 >;
 
-// used for form field configuration object
+/**
+ * Represents the configuration for a time field in a form configuration object.
+ * Extends the base ItemField type to extend field items properties and adding custom properties specific to time fields.
+ *
+ * @interface FieldTime
+ * @extends {ItemField}
+ */
 export interface FieldTime extends ItemField {
-  fieldType: "timePicker";
-  use12Hours: boolean;
-  format: string;
-  defaultValue?: string;
+  fieldType: "timePicker"; // Type of the field (timePicker)
+  use12Hours: boolean; // Whether to use a 12-hour format for the time field
+  format: string; // Format of the time to be displayed
+  defaultValue?: string; // Default value for the time field. Used to set form default values object.
 }
 
-// used for Custom Field Configuration and in case input requires custom props
+/**
+ * Represents the custom field configuration for a time field that is used to determine FieldTime component expected props.
+ * Inherits properties from GeneralFieldConfig and includes if needed, additional properties specific to time fields.
+ *
+ * @interface FieldTimeComponentProps
+ * @extends {CustomFieldConfig}
+ */
 export type FieldTimeComponentProps = {
-  use12Hours: boolean;
-  format: string;
+  use12Hours: boolean; // Whether to use a 12-hour format for the time field
+  format: string; // Format of the time to be displayed
 } & CustomFieldConfig;
