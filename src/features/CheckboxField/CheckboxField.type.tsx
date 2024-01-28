0b1b42cdb64967f4cf73bb39a286f948b3769d2d
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-28 15:07:00 +0200

    add comments to Checkbox Field type file

--- src/features/CheckboxField/CheckboxField.type.tsx
+++ src/features/CheckboxField/CheckboxField.type.tsx
@@ -11,9 +11,23 @@ export type FieldCheckboxT = Customize<
   }
 >;
 
+/**
+ * Represents the configuration for a checkbox field in a form configuration object.
+ * Extends the base ItemField type to extend field items properties and adding custom properties specific to checkbox fields.
+ *
+ * @interface FieldCheckbox
+ * @extends {ItemField}
+ */
 export interface FieldCheckbox extends ItemField {
-  fieldType: "checkbox";
-  defaultChecked: boolean;
+  fieldType: "checkbox"; // Type of the field (checkbox)
+  defaultChecked: boolean; // Default value for the switch field. Used to set form default values object.
 }
 
+/**
+ * Represents the custom field configuration for a checkbox field that is used to determine FieldCheckbox component expected props.
+ * Inherits properties from GeneralFieldConfig and includes if needed, additional properties specific to checkbox fields.
+ *
+ * @interface FieldCheckboxComponentProps
+ * @extends {CustomFieldConfig}
+ */
 export type FieldCheckboxComponentProps = CustomFieldConfig;
