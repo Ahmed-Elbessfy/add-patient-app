601c5fbd4c09c3568a3ce6fe484751df263f4a07
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-28 19:41:58 +0200

    add comments to Phone Field type file

--- src/features/PhoneField/PhoneField.type.tsx
+++ src/features/PhoneField/PhoneField.type.tsx
@@ -3,9 +3,23 @@ import {
   ItemField,
 } from "../AddAppointmentFields/AddAppointmentInputs.type";
 
+/**
+ * Represents the configuration for a phone field in a form configuration object.
+ * Extends the base ItemField type to extend field items properties and adding custom properties specific to phone fields.
+ *
+ * @interface FieldPhone
+ * @extends {ItemField}
+ */
 export interface FieldPhone extends ItemField {
-  fieldType: "phone";
-  defaultValue?: string;
+  fieldType: "phone"; // Type of the field (phone)
+  defaultValue?: string; // Default value for the text field. Used to set form default values object.
 }
 
-export type FieldPhoneComponentProps = FieldPhone & CustomFieldConfig;
+/**
+ * Represents the custom field configuration for a phone field that is used to determine FieldPhone component expected props.
+ * Inherits properties from GeneralFieldConfig and includes if needed, additional properties specific to phone fields.
+ *
+ * @interface FieldPhoneComponentProps
+ * @extends {CustomFieldConfig}
+ */
+export type FieldPhoneComponentProps = CustomFieldConfig;
