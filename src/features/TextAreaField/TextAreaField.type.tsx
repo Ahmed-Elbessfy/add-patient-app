d93855e580d2486722640d2a4d97f9768d3cbf34
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-28 14:58:47 +0200

    add comments to Textarea Field type file

--- src/features/TextAreaField/TextAreaField.type.tsx
+++ src/features/TextAreaField/TextAreaField.type.tsx
@@ -14,16 +14,28 @@ export type FieldTextAreaT = Customize<
   }
 >;
 
-// used for form field configuration object
+/**
+ * Represents the configuration for a textarea field in a form configuration object.
+ * Extends the base ItemField type to extend field items properties and adding custom properties specific to textarea fields.
+ *
+ * @interface FieldTextArea
+ * @extends {ItemField}
+ */
 export interface FieldTextArea extends ItemField {
-  fieldType: "textarea";
-  defaultValue?: string;
-  maxLength?: number;
-  showCount: boolean;
+  fieldType: "textarea"; // Type of the field (textarea)
+  defaultValue?: string; // Default value for the textarea field. Used to set form default values object.
+  maxLength?: number; // Maximum length of the textarea content letters
+  showCount: boolean; // Indicates whether to show character count
 }
 
-// used for Custom Field Configuration and in case input requires custom props
+/**
+ * Represents the custom field configuration for a textarea field that is used to determine FieldTextArea component expected props.
+ * Inherits properties from GeneralFieldConfig and includes if needed, additional properties specific to textarea fields.
+ *
+ * @interface FieldTextAreaComponentProps
+ * @extends {CustomFieldConfig}
+ */
 export type FieldTextAreaComponentProps = {
-  maxLength?: number;
-  showCount: boolean;
+  maxLength?: number; // Maximum length of the textarea content letters
+  showCount: boolean; // Indicates whether to show character count
 } & CustomFieldConfig;
