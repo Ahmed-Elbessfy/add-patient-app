0e4796cda82438a238167eb65fabddbec22f5c27
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-01-28 14:35:18 +0200

    add comments to Number Field type file

--- src/features/NumberField/NumberField.type.tsx
+++ src/features/NumberField/NumberField.type.tsx
@@ -14,17 +14,28 @@ export type FieldNumberT = Customize<
   }
 >;
 
-// used for form field configuration object
+/**
+ * Represents the configuration for a number field in a form configuration object.
+ * Extends the base ItemField type to extend field items properties and adding custom properties specific to number fields.
+ *
+ * @interface FieldNumber
+ * @extends {ItemField}
+ */
 export interface FieldNumber extends ItemField {
-  fieldType: "number";
-  defaultValue?: number;
-  min?: number;
-  max?: number;
+  fieldType: "number"; // Type of the field (text)
+  defaultValue?: number; // Default value for the number field. Used to set form default values object.
+  min?: number; // Minimum allowed value for the number field
+  max?: number; // Maximum allowed value for the number field
 }
 
-// used for Custom Field Configuration and in case input requires custom props
+/**
+ * Represents the custom field configuration for a text field that is used to determine FieldNumber component expected props.
+ * Inherits properties from GeneralFieldConfig and includes if needed, additional properties specific to text fields.
+ *
+ * @interface FieldNumberComponentProps
+ * @extends {CustomFieldConfig}
+ */
 export type FieldNumberComponentProps = {
-  fieldType: "number";
-  min?: number;
-  max?: number;
+  min?: number; // Minimum allowed value for the number field
+  max?: number; // Maximum allowed value for the number field
 } & CustomFieldConfig;
