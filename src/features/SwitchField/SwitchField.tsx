2facf56f32766458fce4aaa726a0fc7a0ac55dec
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-02-01 10:52:12 +0200

    remove label from switch SwitchField
    - currently, no switch field needs label or clarification

--- src/features/SwitchField/SwitchField.tsx
+++ src/features/SwitchField/SwitchField.tsx
@@ -4,21 +4,13 @@ import { useTranslation } from "react-i18next";
 import { FieldSwitchComponentProps } from "./SwitchField.type";
 
 const SwitchField: FC<FieldSwitchComponentProps> = (props) => {
-  const {
-    label,
-    onChange,
-    isDisabled,
-    checkedChildren,
-    unCheckedChildren,
-    value,
-  } = props;
+  const { onChange, isDisabled, checkedChildren, unCheckedChildren, value } =
+    props;
 
   const { t } = useTranslation("translation");
 
   return (
     <>
-      {label && <label>{t(label)}</label>}
-
       <Switch
         disabled={isDisabled}
         checkedChildren={t(checkedChildren)}
