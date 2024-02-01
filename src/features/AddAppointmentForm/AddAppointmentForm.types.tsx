b8e2d40eaeeb29295d29c9bd505f369efb6a452f
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2024-02-01 11:18:41 +0200

    remove unused type

--- src/features/AddAppointmentForm/AddAppointmentForm.types.tsx
+++ src/features/AddAppointmentForm/AddAppointmentForm.types.tsx
@@ -6,12 +6,6 @@ export interface AddAppointmentFormProps {
   onSubmit: (data) => void;
 }
 
-export type ValidationRule = {
-  fieldName: string;
-  date?: string;
-  targetField?: string;
-};
-
 export type DefaultValueObjectFormat = Record<
   string, // string is added only for the dynamic default values object build since name or schema name in nested object contains "." which we use to separate name and build a nested object
   string | number | boolean | undefined | Dayjs
